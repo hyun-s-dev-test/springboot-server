fa86014d85678bd8fd20f8c92cd6c267b07ee75b
0woodev <0woodev@gmail.com>
2022-03-02 22:24:58 +0900

    init: springboot + java + mysql + jpa 스택을 가진 프로젝트 생성

---
+++ src/main/java/com/dev/woo/springbootserver/SpringbootServerApplication.java
@@ -0,0 +1,11 @@
+package com.dev.woo.springbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServerApplication.class, args);
+	}
+}
