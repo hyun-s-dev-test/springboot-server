58dfbf584459ef5ed7e1e4cdafb9265ee9f3d6e6
0woodev <0woodev@gmail.com>
2022-03-03 14:29:15 +0900

    add: UserRepository 테스트 및 직접 만든 findById 도 테스트

--- src/test/java/com/dev/woo/springbootserver/domain/user/UserRepositoryTest.java
+++ src/test/java/com/dev/woo/springbootserver/domain/user/UserRepositoryTest.java
@@ -1,7 +1,47 @@
 package com.dev.woo.springbootserver.domain.user;
 
-import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserRepositoryTest {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @After
+    public void cleanup() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void 유저저장후_불러오기() throws Exception {
+        // given
+        User user = new User("남영우", "0woodev", "password",
+                'M', "none", "");
+        // save
+        userRepository.save(User.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .password(user.getPassword())
+                .gender(user.getGender())
+                .socialType(user.getSocialType())
+                .token(user.getToken())
+                .build());
+            // findById 테스트
+        User savedUser = userRepository.findById(user.getId());
+        assertThat(savedUser.getName()).isEqualTo(user.getName());
+        assertThat(savedUser.getId()).isEqualTo(user.getId());
+    }
 }
