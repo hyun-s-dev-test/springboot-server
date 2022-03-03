ba375d884d101c7ff5d1b9868214ebfa326f9286
0woodev <0woodev@gmail.com>
2022-03-03 15:33:40 +0900

    hotfix: application.properties 에 정보가 올라가지 않도록 git cache 삭제 후 다시 커밋

---
+++ src/main/java/com/dev/woo/springbootserver/domain/term/Term.java
@@ -0,0 +1,34 @@
+package com.dev.woo.springbootserver.domain.term;
+
+import com.dev.woo.springbootserver.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Term extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Builder
+    public Term(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+}
