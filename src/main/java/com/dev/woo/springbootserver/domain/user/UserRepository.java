c37a9f8bb8a808afaf96771dae138cfb724a705a
0woodev <0woodev@gmail.com>
2022-03-03 13:24:28 +0900

    add: UserRepository 제작

--- src/main/java/com/dev/woo/springbootserver/domain/user/UserRepository.java
+++ src/main/java/com/dev/woo/springbootserver/domain/user/UserRepository.java
@@ -1,4 +1,7 @@
 package com.dev.woo.springbootserver.domain.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByPk(Long pk);
 }
