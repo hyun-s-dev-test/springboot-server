66584f746420475bc53677d3ca2d3cf9686c7e93
0woodev <0woodev@gmail.com>
2022-03-03 14:37:38 +0900

    remove: 불필요한 import 제거

--- src/main/java/com/dev/woo/springbootserver/domain/term/TermRepository.java
+++ src/main/java/com/dev/woo/springbootserver/domain/term/TermRepository.java
@@ -1,8 +1,6 @@
 package com.dev.woo.springbootserver.domain.term;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TermRepository extends JpaRepository<Term, Long> {
 }
