eec7e6870604af8ba91c49c3282791588425e2ad
0woodev <0woodev@gmail.com>
2022-03-03 13:09:39 +0900

    add: mysql setting, application.properties 에 추가 및 exclude 제거

--- src/main/java/com/dev/woo/springbootserver/SpringbootServerApplication.java
+++ src/main/java/com/dev/woo/springbootserver/SpringbootServerApplication.java
@@ -2,9 +2,9 @@ package com.dev.woo.springbootserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication // (exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServerApplication.class, args);
