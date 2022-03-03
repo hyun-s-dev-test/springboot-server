832821344781c80d6a56fb990d637a67a4504ce7
0woodev <0woodev@gmail.com>
2022-03-03 15:32:28 +0900

    hotfix: application.properties 에 정보가 올라가지 않도록 git cache 삭제 후 다시 커밋

--- src/main/java/com/dev/woo/springbootserver/domain/term/TermRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dev.woo.springbootserver.domain.term;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TermRepository extends JpaRepository<Term, Long> {
-}
