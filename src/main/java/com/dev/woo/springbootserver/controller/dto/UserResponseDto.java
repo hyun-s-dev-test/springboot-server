741be7093a8db4de5b682db8cfd32fae871770b6
0woodev <0woodev@gmail.com>
2022-03-04 09:49:36 +0900

    add: UserResponseDto Swagger 추가

--- src/main/java/com/dev/woo/springbootserver/controller/dto/UserResponseDto.java
+++ src/main/java/com/dev/woo/springbootserver/controller/dto/UserResponseDto.java
@@ -1,18 +1,30 @@
 package com.dev.woo.springbootserver.controller.dto;
 
 import com.dev.woo.springbootserver.domain.user.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@ApiModel(value = "UserResponseDto", description = "유저조회 정보")
 public class UserResponseDto {
-    private Long pk;
-    private String name;
-    private String id;
-    private char gender;
-    private LocalDate birth;
-    private String socialType;
-    private LocalDateTime createdAt;
+    @ApiModelProperty(value = "pk", notes = "pk", example = "1", required = false)
+    private final Long pk;
+    @ApiModelProperty(value = "name", notes = "이름", example = "남영우", required = false)
+    private final String name;
+    @ApiModelProperty(value = "id", notes = "아이디", example = "0woodev", required = false)
+    private final String id;
+    @ApiModelProperty(value = "gender", notes = "성별", example = "M", required = false)
+    private final char gender;
+    @ApiModelProperty(value = "birth", notes = "생일", example = "1997-07-28", required = false)
+    private final LocalDate birth;
+    @ApiModelProperty(value = "social type", notes = "소셜로그인 종류", example = "none", required = false)
+    private final String socialType;
+    @ApiModelProperty(value = "createdAt", notes = "회원가입시간", example = "YYYY-MM-DD-HH-mm-ss", required = false)
+    private final LocalDateTime createdAt;
 
     public UserResponseDto(User user) {
         this.pk = user.getPk();
