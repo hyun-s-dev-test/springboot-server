f6624a656f4f59967ba2641e69c7cfbb11d538d5
0woodev <0woodev@gmail.com>
2022-03-04 09:48:43 +0900

    add: swagger 설명 추가 및 save API 추가

--- src/main/java/com/dev/woo/springbootserver/controller/UserController.java
+++ src/main/java/com/dev/woo/springbootserver/controller/UserController.java
@@ -4,6 +4,8 @@ package com.dev.woo.springbootserver.controller;
 import com.dev.woo.springbootserver.controller.dto.UserResponseDto;
 import com.dev.woo.springbootserver.controller.dto.UserSaveRequestDto;
 import com.dev.woo.springbootserver.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "User Controller", tags = "User")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
@@ -21,7 +24,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "모든 유저 조회", description = "모든 유저의 정보를 가져옵니다.(비밀번호, 토큰 제외)")
+    @ApiOperation(value = "모든 유저 조회", notes = "모든 유저의 정보를 가져옵니다.(비밀번호, 토큰 제외)")
     @ApiResponses({
             @ApiResponse(code = 200, message = "OK!"),
             @ApiResponse(code = 400, message = "BAD REQUEST!"),
@@ -30,7 +33,7 @@ public class UserController {
     })
     @GetMapping("/all")
     public ResponseEntity<List<UserResponseDto>> getAllUserInfo() {
-        return new ResponseEntity<>(userService.findAllUser(), HttpStatus.OK);
+        return new ResponseEntity<List<UserResponseDto>>(userService.findAllUser(), HttpStatus.OK);
     }
 
     @Operation(summary = "아이디로 유저 조회", description = "입력한 아이디와 동일한 유저의 정보를 가져옵니다.(비밀번호, 토큰 제외)")
@@ -42,9 +45,28 @@ public class UserController {
     })
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> getUserInfoById(@PathVariable String id) {
-        UserResponseDto userResponseDto = userService.findUserById(id);
+        UserResponseDto userResponseDto = null;
 
-        return new ResponseEntity<>(userResponseDto,
-                (userResponseDto != null) ? HttpStatus.OK : HttpStatus.NO_CONTENT);
+        try {
+             userResponseDto = userService.findUserById(id);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>((UserResponseDto)null, HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>((UserResponseDto)null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
+
+    @Operation(summary = "유저 추가", description = "입력한 데이터로 새로운 유저를 생성합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!"),
+            @ApiResponse(code = 400, message = "BAD REQUEST!"),
+            @ApiResponse(code = 404, message = "NOT FOUND!"),
+            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR!"),
+    })
+    @PostMapping()
+    public Long save(@RequestBody UserSaveRequestDto requestDto) {
+        return userService.save(requestDto);
     }
 }
